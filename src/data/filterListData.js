c5a4a0834ba51238ae10d9b8b4e1ee56dea66153
Juan José Martínez <jmartinezc.cp@gmail.com>
2026-02-10 03:53:22 -0300

    Se genera código para mostrar GameCards en JSX Categorías.

---
+++ src/data/filterListData.js
@@ -0,0 +1,34 @@
+const filterListData = [
+    {
+        _id: 1,
+        name: 'Todos',
+        active: true,
+    },
+    {
+        _id: 2,
+        name: 'RPG',
+        active: false,
+    },
+    {
+        _id: 3,
+        name: 'MOBA',
+        active: false,
+    },
+    {
+        _id: 4,
+        name: 'Batalla',
+        active: false,
+    },
+    {
+        _id: 5,
+        name: 'Carreras',
+        active: false,
+    },
+    {
+        _id: 6,
+        name: 'Peleas',
+        active: false,
+    },
+];
+
+export default filterListData;
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./categories.css";
+import filterListData from "../data/filterListData";
+import GameCard from "../components/GameCard";
 
 function Categories({ games, reference }) {
+  const [data, setData] = useState(games);
+
+  const [filters, setFilters] = useState(filterListData);
+  const handleFilterGames = (category) => {
+    setFilters(
+      filters.map((filter) => {
+        filter.active = false;
+        if (filter.name === category) {
+          filter.active = true;
+        }
+        return filter;
+      }),
+    );
+  };
   return (
     <section id="categories" className="categories" ref={reference}>
-      <h1>Categorías</h1>
+      <div className="container-fluid mt-2">
+        <div className="row">
+          <div className="col-lg-8 d-flex align-items-center justify-content-start">
+            <ul className="filters">
+              {filters.map((filter) => (
+                <li
+                  key={filter._id}
+                  className={`${filter.active ? "active" : undefined}`}
+                  onClick={() => handleFilterGames(filter.name)}
+                >
+                  {filter.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-lg-4 d-flex align-items-center justify-content-end">
+            <div className="search">
+              <i className="bi bi-search"></i>
+              <input type="text" name="search" placeholder="Buscar" />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {games.map((game) => (
+            <GameCard key={game._id} game={game} />
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
